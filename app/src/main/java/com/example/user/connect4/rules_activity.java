6fdf035a97b8224ea4f3a5de2f0d0b3a6f66847e
johurulsec <johircse37@gmail.com>
2018-08-04 13:09:09 +0600

    Add files via upload

---
+++ app/src/main/java/com/example/user/connect4/rules_activity.java
@@ -0,0 +1,28 @@
+package com.example.user.connect4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by JOHURUL on 04-Aug-18.
+ */
+
+public class rules_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_rules);
+    }
+    public void back(View v)
+    {
+        finish();
+    }
+}
